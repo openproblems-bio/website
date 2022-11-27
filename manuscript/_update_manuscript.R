2f7db02214ecff1df9807fa1695d096fa5cdd277
Robrecht Cannoodt <rcannood@gmail.com>
2022-11-27 11:49:25 +0100

    fetch manuscript from google drive

--- manuscript/_update_manuscript.R
+++ manuscript/_update_manuscript.R
@@ -7,13 +7,14 @@ temp_manuscript <- tempfile()
 dest_manuscript <- "manuscript/index.qmd"
 dest_library <- "manuscript/library.bib"
 
-if (FALSE) {
+# todo: somehow detect whether drive can access a google drive token
+if (Sys.info()[["user"]] == "rcannood") {
   library(googledrive)
   library(textreadr)
 
   # httr::set_config(httr::config(http_version = 0)) # avoid http2 framing layer bug
   drive <- drive_download(
-    as_id("1FqmINEd4miXlXvIR2JLZhu2n8AIU_5h3gc_5QoCmRsQ"),
+    as_id("1gcfToxQ5cVgHfQ5Z6M0mbaCzUISEC-CQt352CXvP4s4"),
     type = "docx",
     overwrite = TRUE,
     path = temp_manuscript
@@ -27,17 +28,16 @@ if (FALSE) {
     # add spaces before citations
     # str_replace_all("([^ ])(\\[@[^\\]]*\\])", "\\1 \\2") %>% 
     # add newlines before sections
-    str_replace_all("^#", "\n#")# %>%
-    #write_lines(dest_manuscript)
+    str_replace_all("^#", "\n#") %>%
+    write_lines(dest_manuscript)
 }
 
 # convert references
 d2b <- reticulate::import("doi2bib")
 
-manu <- read_lines(dest_manuscript) %>% paste(collapse = "\n")
-
 # extract citation yaml
-citations_yaml <- gsub("```\\{yaml[^\\}]*\\}\n(citations:[^`]*)```", "\\1", manu)
+manu_txt <- read_lines(dest_manuscript) %>% paste(collapse = "\n")
+citations_yaml <- gsub(".*```\\{citations([^`]*)```.*", "\\1", manu_txt)
 citations <- yaml::read_yaml(text = citations_yaml)$citations
 
 bibs <- map2_chr(names(citations), citations, function(name, text) {
