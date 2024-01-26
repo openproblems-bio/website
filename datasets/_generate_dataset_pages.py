2f7c03e537b4dd727f6960844dd1b46724c1f319
Kai Waldrant <kai@data-intuitive.com>
2024-01-26 10:25:49 +0100

    Add related benchmark table

--- datasets/_generate_dataset_pages.py
+++ datasets/_generate_dataset_pages.py
@@ -2,13 +2,13 @@ from pathlib import Path
 import json
 
 print("Fetch template", flush=True)
-benchmark_dir = Path("datasets/")
+dataset_dir = Path("datasets/")
 
-dataset_info_files = benchmark_dir.glob("*/*/meta.json")
+dataset_meta_files = dataset_dir.glob("*/*/meta.json")
 
-for dataset_info_file in dataset_info_files:
-    print(f"Reading {dataset_info_file}", flush=True)
-    dataset_info = json.loads(dataset_info_file.read_text())
+for dataset_file in dataset_meta_files:
+    print(f"Reading {dataset_file}", flush=True)
+    dataset_info = json.loads(dataset_file.read_text())
     info_uns = dataset_info.get("uns")
     dataset_name = info_uns.get("dataset_name", "<Name missing>")
     dataset_summary = info_uns.get("dataset_summary", "<Summary missing>")
@@ -44,7 +44,7 @@ params <- list(data_file = "./meta.json")
 {{{{< include ../../_include/_task_template.qmd >}}}}
 """
 
-    index_qmd = dataset_info_file.parent / "index.qmd"
+    index_qmd = dataset_file.parent / "index.qmd"
     print(f"Write to {index_qmd}", flush=True)
     with index_qmd.open("w", encoding ="utf-8") as f:
         f.write(content)
