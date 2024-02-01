96c8762617769a07d6722c49979aeb3edacdb447
Kai Waldrant <kai@data-intuitive.com>
2024-02-01 16:39:21 +0100

    add multimodal datasets

--- datasets/_generate_dataset_pages.py
+++ datasets/_generate_dataset_pages.py
@@ -1,30 +1,35 @@
 from pathlib import Path
+import os
 import json
 
 print("Fetch template", flush=True)
 dataset_dir = Path("datasets/")
 
-dataset_meta_files = dataset_dir.glob("*/*/meta.json")
+dataset_meta_files = dataset_dir.glob("*/*/*meta*.json")
 
-for dataset_file in dataset_meta_files:
-    print(f"Reading {dataset_file}", flush=True)
-    dataset_info = json.loads(dataset_file.read_text())
-    info_uns = dataset_info.get("uns")
-    dataset_name = info_uns.get("dataset_name", "<Name missing>")
-    dataset_summary = info_uns.get("dataset_summary", "<Summary missing>")
-    dataset_id = info_uns.get("dataset_id", "<ID missing>")
-    dataset_ref = info_uns.get("dataset_reference", "<Ref missing>")
+parent_dirs = [Path(file).parent for file in dataset_meta_files]
+parent_dirs = list(set(parent_dirs))
 
-    dataset_loader = dataset_id.split("/")[0]
+for dir in parent_dirs:
+  meta_files = list(dir.glob("*meta*.json"))  
+  print(f"Reading {meta_files[0]}", flush=True)
+  dataset_info = json.loads(meta_files[0].read_text())
+  info_uns = dataset_info.get("uns")
+  dataset_name = info_uns.get("dataset_name", "<Name missing>")
+  dataset_summary = info_uns.get("dataset_summary", "<Summary missing>")
+  dataset_id = info_uns.get("dataset_id", "<ID missing>")
+  dataset_ref = info_uns.get("dataset_reference", "<Ref missing>")
 
-    content = f"""\
+  dataset_loader = dataset_id.split("/")[0]
+  str_files = [str(file.name) for file in meta_files]
+  data_files=', '.join(f'"{file}"' for file in str_files)
+  content = f"""\
 ---
 title: "{dataset_name}"
 subtitle: "{dataset_summary}"
 categories: ["{dataset_loader}"]
-title-block-banner: true
-title-block-banner-color: white
-css: ../../../events/events.css
+title-block-banner: transparent
+css: [../../../events/events.css, ../../datasets.css]
 toc: false
 engine: knitr
 citation-location: document
@@ -34,15 +39,21 @@ dataset-id: {dataset_id}
 dataset-ref: "@{dataset_ref}"
 ---
 
+```{=html}
+<img src="../../../images/hero/anndata_schema.png" alt="Anndata Schema" class="dataset_img">
+```
+
+
 ```{{r}}
 #| include: false
-params <- list(data_file = "./meta.json")
+meta_files <- c({data_files})
+params <- list(data_file = lapply(meta_files, function(x) paste0("./", x)))
 ```
 
 {{{{< include ../../_include/_task_template.qmd >}}}}
 """
 
-    index_qmd = dataset_file.parent / "index.qmd"
-    print(f"Write to {index_qmd}", flush=True)
-    with index_qmd.open("w", encoding ="utf-8") as f:
-        f.write(content)
+  index_qmd = dir / "index.qmd"
+  print(f"Write to {index_qmd}", flush=True)
+  with index_qmd.open("w", encoding ="utf-8") as f:
+      f.write(content)
