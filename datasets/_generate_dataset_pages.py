064b5b76fd6440406a52ad0622ef12d4160d5ef1
Kai Waldrant <kai@data-intuitive.com>
2024-02-12 23:47:45 +0100

    Add dataset pages (#303)
    * WIP
    
    * update grid metadata to 3 col
    
    * WIP concept dataset page
    
    * update OP
    
    * update git ignore
    
    * WIP template
    
    * Update dataset main listing
    
    * WIP add sidebar
    
    * refactor use of margin-bar
    
    * fix margin unclickable links
    
    * Add related benchmark table
    
    * filter metadata
    
    * Update dataset template to account for real data
    
    * Add dataset pages
    
    * update datasets
    
    * add description to fields
    
    * add multimodal datasets
    
    * fix image and add dimension
    
    * update datasets
    
    * move data files
    
    * use absolute paths in dataset qmd files
    
    * refactor index template
    
    * refactor
    
    * fix dataset index template
    
    * use gt to render tables
    
    * update dataset info page
    
    * update dataset listing
    
    * add dataset hero
    
    * refactor image-height
    
    * fix categories overflow
    
    * fix title centering
    
    * switch back to knitr::kable but use tbl-colwidths to fix column widths
    
    * update code css specificity
    
    * flipped font weight on listing cards
    
    * Fix dark/light theme code block issues
    
    * Align the v1 dataset ids with the v2 dataset ids (#306)
    
    * align dataset ids using source_dataset_id
    
    * fix more source dataset ids
    
    * refactor bottom border
    
    * fix datasets table
    
    * update dataset css
    
    * fix some darkmode css
    
    * Update table header
    
    * WIP try break word; try setting table col width;
    
    * add wbr
    
    ---------
    
    Co-authored-by: Robrecht Cannoodt <rcannood@gmail.com>

---
+++ datasets/_generate_dataset_pages.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import json
+
+print("Fetch template", flush=True)
+results_dir = Path("results/")
+datasets_dir = Path("datasets/")
+
+dataset_dirs = [state.parent.parent for state in datasets_dir.glob("**/data/state.yaml")]
+
+for dir in dataset_dirs:
+  print(f"Processing {dir}", flush=True)
+  meta_files = list(dir.glob("data/*.json"))
+
+  print(f"  Reading {meta_files[0]}", flush=True)
+  dataset_info = json.loads(meta_files[0].read_text(encoding="UTF-8"))
+  info_uns = dataset_info.get("uns")
+  dataset_name = info_uns.get("dataset_name", "<Name missing>")
+  dataset_summary = info_uns.get("dataset_summary", "<Summary missing>")
+  dataset_id = info_uns.get("dataset_id", "<ID missing>")
+  dataset_ref = info_uns.get("dataset_reference", "<Ref missing>")
+
+  dataset_loader = dataset_id.split("/")[0]
+  results_dir = "../" * len(dir.parts) + "results"
+  content = f"""\
+---
+dataset-id: "{dataset_id}"
+title: "{dataset_name}"
+subtitle: "{dataset_summary}"
+categories: ["{dataset_loader}"]
+css: [/datasets/datasets.css]
+title-block-banner: transparent
+toc: false
+engine: knitr
+citation-location: document
+---
+
+```{{r}}
+#| include: false
+params <- list(data_dir = "{dir}/data", results_dir = "results")
+params <- list(data_dir = "./data", results_dir = "{results_dir}")
+```
+
+{{{{< include /datasets/_include/_index_template.qmd >}}}}
+"""
+
+  index_qmd = dir / "index.qmd"
+  print(f"  Writing to {index_qmd}", flush=True)
+  with index_qmd.open("w", encoding ="utf-8") as f:
+      f.write(content)
