2a84557169c515634620d1b7bc0f21abbb230446
Kai Waldrant <kai@data-intuitive.com>
2024-11-25 23:12:48 +0100

    Update method and metric json files

--- results/_transform_results.py
+++ results/_transform_results.py
@@ -20,6 +20,13 @@ def read_json_file(file_path):
     
     return data
 
+def write_json_file(json_obj, file):
+
+    path = Path(file)
+    
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(json_obj, f, indent=2)
+
 dirs = [dir for dir in Path("results").iterdir() if dir.is_dir()]
 
 exclude_dirs = ["_include", "perturbation_prediction", "spatially_variable_genes", "foundation_models"]
@@ -34,31 +41,27 @@ for dir in dirs:
 
     # Transform the method_info.json file
     method_info_path = data_path.joinpath("method_info.json")
+    print("Reading method json", flush=True)
     method_info = read_json_file(method_info_path)
+    print("processing method json", flush=True)
     for info in method_info:
         info["code_url"] = info["code_url"] + "/tree/v1.0.0/openproblems/tasks"
         info["implementation_url"] = info["implementation_url"].replace("main", "v1.0.0")
         info["code_version"] = "v1.0.0"
         info["image"] = "https://github.com/openproblems-bio/openproblems/pkgs/container/" + info["image"]
 
+    print("Writing method json", flush=True)
+    write_json_file(method_info, method_info_path)
+
+    print("Reading metric json", flush=True)
     metric_info_path = data_path.joinpath("metric_info.json")
     metric_info = read_json_file(metric_info_path)
+    print("processing metric json", flush=True)
     for info in metric_info:
         info["implementation_url"] = info["implementation_url"].replace("main", "v1.0.0")
         info["code_version"] = "v1.0.0"
         info["image"] = "https://github.com/openproblems-bio/openproblems/pkgs/container/" + info["image"]
 
-    # Transform the task_info.json file
-    task_info_path = data_path.joinpath("task_info.json")
-    task_info = read_json_file(task_info_path)
-    for info in task_info:
-        info["repo"]= "https://github.com/" + info["repo"] + "/tree/v1.0.0"
-        info["version"] = "v1.0.0"
-        info["license"] = "MIT"
-        
-        
-        info["authors"] = [{"name": "OpenProblems", "roles": ["maintainer"], "info": {"github": "openproblems-bio"}}]
-        
+    print("Writing metric json", flush=True)
+    write_json_file(metric_info, metric_info_path)
 
-    
-    # Read the task_info.json file
